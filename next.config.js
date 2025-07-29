63543f0f7fdc13fe2a6e3ca0c8d1f81e9f87d7a1
tsewanglhamo <tlhamo3@wisc.edu>
2025-07-29 15:51:22 -0400

    Update config for static export deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
-    domains: ['localhost'],
-  },
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig 
